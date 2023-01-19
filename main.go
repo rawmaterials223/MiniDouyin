5cd64453fd41c1b762d9c746513a32df49a74e1b
rawmaterials223 <lulingyi17010223@hotmail.com>
2023-01-19 17:45:13 +0800

    add repository

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rawmaterials223/MiniDouyin/repository"
+	"github.com/rawmaterials223/MiniDouyin/service"
 )
 
 func main() {
+	go service.RunMessageServer()
+
+	if err := Init(); err != nil {
+		return
+	}
+
 	r := gin.Default()
+
 	initRouter(r)
-	r.Run()
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func Init() error {
+	if err := repository.DbInit(); err != nil {
+		return err
+	}
+
+	return nil
 }
